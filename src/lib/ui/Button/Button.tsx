ebb5f87844dbc27a638bb3a22ce23a6c5449830a
AdaoBJr <adaobjr@gmail.com>
2022-07-10 08:20:14 -0400

    style: :sparkles: added ButtonMUI on Button

--- src/lib/ui/Button/Button.tsx
+++ src/lib/ui/Button/Button.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Button as ButtonMUI } from '@mui/material';
+
 import { ReactFC } from 'types/interface';
 
-export const Button:React.FC<ReactFC> = ({children}) => {
-  return <button>{children}</button>;
-};
+export const Button: React.FC<ReactFC> = ({ children }) => (
+  <ButtonMUI variant="contained">{children}</ButtonMUI>
+);
