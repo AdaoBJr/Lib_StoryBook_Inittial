359df694d0b4098f594b97bcebff572a95875d7c
AdaoBJr <adaobjr@gmail.com>
2022-07-10 10:17:40 -0400

    feat: :boom: created Button.stories on Stories

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { Button } from '../src/lib/ui';
+
+storiesOf('Button', module).add('Default', () => <Button>Teste</Button>);
